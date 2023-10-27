01b5b1f5cfae9796e1e0987e3c2ce25f24094930
ashikhvu <vuashikh16@gmail.com>
2023-10-27 09:13:16 +0000

    fourth commit

---
+++ zohoapp/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import AddItem,Purchase,Sales, SalesOrder,Unit,Journal,JournalEntry,JournalComment,transactions
+
+# Register your models here.
+admin.site.register(AddItem)
+admin.site.register(Purchase)
+admin.site.register(Sales)
+admin.site.register(Unit)
+admin.site.register(SalesOrder)
+admin.site.register(transactions)
+
+#.....................mirna.............................manual journal..................................................
+
+
+@admin.register(Journal)
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('journal_no', 'date', 'status')
+    actions = ['publish_journals']
+
+    def publish_journals(self, request, queryset):
+        queryset.update(status='published')
+        self.message_user(request, 'Selected journals have been published.')
+    publish_journals.short_description = 'Publish selected journals'  
+
+admin.site.register(JournalEntry)
+
+@admin.register(JournalComment)
+class JournalCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'journal', 'date_time', 'text')
